56fd558b8b2c848507d4ee5f0cb80e43f001035c
charish37 <charishmanadipalli77@gmail.com>
2025-03-31 21:32:21 -0400

    Initial Spring Boot project setup

---
+++ foodDeliveryWebsite/src/main/java/com/project/two/foodDeliveryWebsite/FoodDeliveryWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.project.two.foodDeliveryWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryWebsiteApplication.class, args);
+	}
+
+}
---
+++ foodDeliveryWebsite/src/test/java/com/project/two/foodDeliveryWebsite/FoodDeliveryWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.two.foodDeliveryWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
